9ccf78e773192bde8a299556d41c82f5dab3625a
Naman Rajput <namanrajput801@gmail.com>
2023-10-11 11:44:03 +0530

    ui: changes in login page and make register page from scratch

--- script/script.js
+++ script/script.js
@@ -10,3 +10,15 @@ function myFunction() {
 function hide() {
   document.getElementById("myDIV").style.display = "none";
 }
+
+// for register page
+$("#chooseFile").bind("change", function () {
+  var filename = $("#chooseFile").val();
+  if (/^\s*$/.test(filename)) {
+    $(".file-upload").removeClass("active");
+    $("#noFile").text("No file chosen...");
+  } else {
+    $(".file-upload").addClass("active");
+    $("#noFile").text(filename.replace("C:\\fakepath\\", ""));
+  }
+});
